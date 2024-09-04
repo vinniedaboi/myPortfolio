834c0f66e2b2c71d8887ddad6a3ae86a0b6d1ebc
vinniedaboi <kidosol5224@gmail.com>
2024-09-04 14:55:01 +0800

    edited config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Vincent Ng | Personal Portfolio Website';
-export const SITE_DESCRIPTION = 'My personal portfolio';
+export const SITE_DESCRIPTION = 'My portfolio and blog! :)';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
